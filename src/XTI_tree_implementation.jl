f19604f85e2f2596e7600a7399fb79760084286b
Patrik Gerber <patrik.r.gerber@gmail.com>
2020-11-29 17:54:39 -0500

    Algorithm works on toy problem. Whats left is implementing initialization

--- src/XTI_tree_implementation.jl
+++ src/XTI_tree_implementation.jl
@@ -1,30 +1,31 @@
 using BenchmarkTools
+using OptimalTransport
 
 ####################### TEST CASE 1 ###########################
 
-parents = [0,1,7,3,6,1,6,2,7]
-thread = [2,8,4,1,7,5,9,6,3]
-rev_thread = [4,1,9,3,6,8, 5,2,7]
-succ_num = [9,2,2,1,1,6,4,1,1]
-last_succ = [4,8,4,4,5,4,4,8,9]
-
-(u,v) = (9,8)
-(p,q) = (6,7)
-
-# Correct results after step III
-parents3 = [0,1,7,3,6,1,9,2,0]
-thread3 = [2, 8, 4, 9, 1, 5, 3, 6, 7]
-rev_thread3 = [5, 1, 7, 3, 6, 8, 9, 2, 4]
-succ_num3 = [5, 2, 2, 1, 1, 2, 3, 1, 4]
-last_succ3 = [5, 8, 4, 4, 5, 5, 4, 8, 4]
-
-
-# Correct results after step IV
-parents4 = [0, 1, 7, 3, 6, 1, 9, 2, 8]
-thread4 = [2,8,4,6,1,5,3,9,7]
-rev_thread4 = [5, 1, 7, 3, 6, 4, 9, 2, 8]
-last_succ4 = [5, 4, 4, 4, 5, 5, 4, 4, 4]
-succ_num4 = [9, 6, 2, 1, 1, 2, 3, 5, 4]
+# parents = [0,1,7,3,6,1,6,2,7]
+# thread = [2,8,4,1,7,5,9,6,3]
+# rev_thread = [4,1,9,3,6,8, 5,2,7]
+# succ_num = [9,2,2,1,1,6,4,1,1]
+# last_succ = [4,8,4,4,5,4,4,8,9]
+#
+# (u,v) = (9,8)
+# (p,q) = (6,7)
+#
+# # Correct results after step III
+# parents3 = [0,1,7,3,6,1,9,2,0]
+# thread3 = [2, 8, 4, 9, 1, 5, 3, 6, 7]
+# rev_thread3 = [5, 1, 7, 3, 6, 8, 9, 2, 4]
+# succ_num3 = [5, 2, 2, 1, 1, 2, 3, 1, 4]
+# last_succ3 = [5, 8, 4, 4, 5, 5, 4, 8, 4]
+#
+#
+# # Correct results after step IV
+# parents4 = [0, 1, 7, 3, 6, 1, 9, 2, 8]
+# thread4 = [2,8,4,6,1,5,3,9,7]
+# rev_thread4 = [5, 1, 7, 3, 6, 4, 9, 2, 8]
+# last_succ4 = [5, 4, 4, 4, 5, 5, 4, 4, 4]
+# succ_num4 = [9, 6, 2, 1, 1, 2, 3, 5, 4]
 
 
 
@@ -99,7 +100,9 @@ function STEP_III(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v, 
     p_x = parents[x]
     son_x = -1
     y = rev_thread[x]
-    f_p_x = last_succ[p_x]
+    if p_x > 0
+        f_p_x = last_succ[p_x]
+    end
 
     dirty_revs = []
 
@@ -190,8 +193,6 @@ function update(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v)
     ################# STEP I #################
     STEP_I(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v)
 
-    # Update the potentials of T(q)
-
     x₁ = 1
     x₂ = q
     y₁ = v
@@ -199,30 +200,21 @@ function update(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v)
     ################# STEP III ###############
     STEP_III(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v, x₁, x₂, y₁, y₂)
 
+    # Update the potentials of T(q). Here T(q) is rooted at y₂
+    curr = y₂
+    prev = y₁
+    tmp = last_succ[y₂]
+    while prev != tmp
+        _potential[curr] = cost(curr, prev) - _potential[prev]
+        prev, curr = curr, thread[curr]
+    end
 
     ############### STEP IV #################
     STEP_IV(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v, x₁, x₂, y₁, y₂)
 end
 
-@time update(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v)
-println(parents == parents4 && thread == thread4 && rev_thread == rev_thread4 && last_succ == last_succ4 && succ_num == succ_num4)
-
-
-n=2
-m=3
-_cost = (randn(2) .- randn(3)').^2
-a = ones(1, n)./n
-b = ones(1, m)./m
-_flow = [1/3 1/6 0; 0 1/6 1/3]
-_f = [0, cost(2,4)-cost(1,4)]
-_g = [cost(1,3),cost(1,4), cost(2,5) - (cost(2,4) - cost(1,4))]
-# println(_f .+ _g' - _cost)
-
-parents = [0,4,1,1,2]
-thread = [4,5,1,2,3]
-rev_thread = [3, 4, 5, 1, 2]
-succ_num = [5, 2, 1, 3, 1]
-last_succ = [3, 5, 3, 5, 5]
+# @time update(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v)
+# println(parents == parents4 && thread == thread4 && rev_thread == rev_thread4 && last_succ == last_succ4 && succ_num == succ_num4)
 
 
 # _flow = n × m (Sparse) Matrix representing the flow.
@@ -233,6 +225,18 @@ end
 function cost(a, b)
     return _cost[min(a,b), max(a,b)-n]
 end
+
+function potential(a)
+    tmp = a-n
+    if a < n+1 && a > 0
+        return _f[a]
+    elseif 0 < tmp && tmp < m+1
+        return _g[tmp]
+    else
+        throw(error("a needs to be in [n+m]"))
+    end
+end
+
 
 function f(a)
     if a < 1 || a > n
@@ -249,20 +253,20 @@ function g(a)
     return _g[tmp]
 end
 
+# Returns the two nodes between which the entering edge lies
 function find_entering()
     curr_min = 0
     enter1 = enter2 = -1
     for i in 1:n
         for j in 1:m
-            tmp = _cost[i,j] - _f[i] - _g[j]
-            if tmp < currmin
-                currmin = tmp
-                if i == parent
-                enter1, enter2 = i,j
+            tmp = _cost[i,j] - _potential[i] - _potential[n+j]
+            if tmp < curr_min
+                curr_min = tmp
+                enter1, enter2 = i,j+n
             end
         end
     end
-    return i,j
+    return enter1, enter2
 end
 
 # Input = (enter1, enter2) the edge being added and flow where flow[(i-1)*m+j] is the flow between (i,j)
@@ -272,37 +276,109 @@ function find_leaving(enter1, enter2, parents, succ_num)
         left,right = right, left
     end
     left_δ = right_δ = 1
-    enter_left = enter_right = -1
-    state = -1
+    left_left = left_right = right_left = right_right = -1
     while left != right
         if succ_num[left] > succ_num[right]
             p_right = parents[right]
             if p_right < right
                 candidate_δ = flow(p_right, right)
                 if candidate_δ < right_δ
-                    state = 1
                     right_δ = candidate_δ
-                    enter_left = p_right
-                    enter_right = right
+                    right_right = right
+                    right_left = p_right
                 end
             end
             right = p_right
         else
-            left = parents_left
+            p_left = parents[left]
             if left < p_left
                 candidate_δ = flow(left, p_left)
                 if candidate_δ <= left_δ
-                    state = 0
                     left_δ = candidate_δ
-                    enter_left = left
-                    enter_right = p_left
+                    left_left = left
+                    left_right = p_left
                 end
             end
             left = p_left
         end
     end
+    left,right = enter1, enter2
+    if enter1 > enter2
+        left,right = right, left
+    end
     if left_δ <= right_δ
-        return left_δ, enter_left, enter_right
+        (u,v,p,q) = (left, right, left_right, left_left)
+        return left_δ, u, v, p, q
+    else
+        (u,v,p,q) = (right, left, right_left, right_right)
+        return right_δ, u, v, p, q
+    end
+end
 
-    return δ, state, enter_left, enter_right
+function update_flow(u,v,δ, parents, succ_num)
+    left,right = u, v
+    if u > v
+        left,right = right, left
+    end
+    println("LOL Adding δ to edge ", (left, right))
+    _flow[left, right - n] = δ
+    while left != right
+        if succ_num[left] > succ_num[right]
+            p_right = parents[right]
+            if p_right < right
+                println("Taking δ from edge ", (p_right, right))
+                _flow[p_right, right-n] -= δ
+            else
+                println("Adding δ to edge ", (right, p_right))
+                _flow[right, p_right - n] += δ
+            end
+            right = p_right
+        else
+            p_left = parents[left]
+            if left < p_left
+                println("Taking δ from edge ", (left, p_left))
+                _flow[left, p_left-n] -= δ
+            else
+                println("Adding δ to edge ", (p_left, left))
+                _flow[p_left, left-n] += δ
+            end
+            left = p_left
+        end
+    end
+end
+
+n=2
+m=3
+_cost = (randn(2) .- randn(3)').^2
+a = ones(1, n)./n
+b = ones(1, m)./m
+_flow = [1/3 1/6 0; 0 1/6 1/3]
+# _f = [0, cost(2,4)-cost(1,4)]
+# _g = [cost(1,3),cost(1,4), cost(2,5) - (cost(2,4) - cost(1,4))]
+_potential = [0, cost(2,4)-cost(1,4), cost(1,3),cost(1,4), cost(2,5) - (cost(2,4) - cost(1,4))]
+# println(_cost - (_potential[1:n] .+ _potential[end-m:end]'))
+
+parents = [0,4,1,1,2]
+thread = [4,5,1,2,3]
+rev_thread = [3, 4, 5, 1, 2]
+succ_num = [5, 2, 1, 3, 1]
+last_succ = [3, 5, 3, 5, 5]
+
+# (u,v,p,q) = (5,1,1,4)
+# update(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v)
+
+
+
+@time γ = OptimalTransport.emd(vec(a), vec(b), _cost)
+
+@time while true
+    enter1, enter2 = find_entering()
+    δ, u, v, p, q = find_leaving(enter1, enter2, parents, succ_num)
+
+    if u > 0 && (u,v) != (q,p)
+        update_flow(u, v, δ, parents, succ_num)
+        update(parents, thread, rev_thread, succ_num, last_succ, p, q, u, v)
+    else
+        break
+    end
 end
