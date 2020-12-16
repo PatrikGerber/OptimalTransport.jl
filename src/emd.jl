36e722b360708ceb91f39c5aa725db46157e3339
PatrikGerber <patrik.r.gerber@gmail.com>
2020-12-16 19:11:14 +0100

    Cleaned up code, as handed in

--- src/emd.jl
+++ src/emd.jl
@@ -32,7 +32,7 @@ end
 
 # Given the tree defined by left & right, calculates the dual potentials u,v, in place
 # using DFS. Pretty sure that this function actually works as intended.
-function updatePotential!(u, v, C, left, right, state, max_iter = 10000)
+function update_potential!(u, v, C, left, right, state, max_iter = 10000)
     n = length(u)
     m = length(v)
     # Since tree structure might in fact be a forest, need to start DFS from
@@ -40,7 +40,6 @@ function updatePotential!(u, v, C, left, right, state, max_iter = 10000)
     # nodes on the left of the bipartite graph in 'unchanged'.
     sgn = state[1]
     for i in 1:n
-        # println("Outside")
         if !(state[i] == sgn)
             continue
         end
@@ -50,22 +49,20 @@ function updatePotential!(u, v, C, left, right, state, max_iter = 10000)
 end
 
 
-# Helper for updatePotential(). side = {0,1} = {left,right}. Basically a standard DFS implementation
+# Helper for update_potential(). side = {0,1} = {left,right}. Basically a standard DFS implementation
 function DFS_potential!(node, side, u, v, C, left, right, state, sgn)
-    # println(unchanged)
     if side == 0
         # If have already visited this node, then return
         if state[node] != sgn
             return
         end
         state[node] = !state[node]
-        for neighbour in left[node] # println("Looking at right ", neighbour, " from left ", node, " unchanged = ", unchanged) v[neighbour] = C[node,neighbour] - u[node]
+        for neighbour in left[node]
             v[neighbour] = C[node,neighbour] - u[node]
             DFS_potential!(neighbour, 1, u, v, C, left, right, state, sgn)
         end
     else
         for neighbour in right[node]
-            # println("Looking at left ", neighbour, " from right ", node, " unchanged = ", unchanged)
             u[neighbour] = C[neighbour, node] - v[node]
             DFS_potential!(neighbour, 0, u, v, C, left, right, state, sgn)
         end
@@ -73,10 +70,11 @@ function DFS_potential!(node, side, u, v, C, left, right, state, sgn)
 end
 
 
-# We find the entering edge by finding the minimal difference C - u - v over
-# all edges. Currently not implementing block pivoting.
+# Finds the entering edge by finding the minimal difference C - u - v over
+# all edges. 1 ≦ blk_size ≦ mn is the size of the block used in block search,
+# while 1 ≦ blk_start ≦ mn is the starting edge of the block search. (bad_i[k], bad_j[k])ₖ
+# is a list of edges to avoid because they have been tried and don't improve the objective.
 function find_entering(u, v, C, blk_size, blk_start, res, bad_i, bad_j, bad_index)
-    # println((blk_size, blk_start))
     n = length(u)
     m = length(v)
     currmin = 0
@@ -118,6 +116,8 @@ function find_entering(u, v, C, blk_size, blk_start, res, bad_i, bad_j, bad_inde
     res[3] = 0
 end
 
+# Given arrays bad_i, bad_j and (i,j) returns true if (i,j) == (bad_i[k], bad_j[k])
+# for some k, and false otherwise.
 function valid(bad_i, bad_j, bad_index, i, j)
     for k in 1:bad_index
         if (bad_i[k], bad_j[k]) == (i,j)
@@ -149,14 +149,9 @@ function find_cycle(tail, head, left, right, left_parents, right_parents, node_q
         if side
             for neighbour in right[node]
                 # Need the next condition to avoid turning around straight away
-                # ERROR HERE
-                # if neighbour == tail
-                    # println("HERE ", right_parents[node], " ", neighbour)
-                # end
                 if right_parents[node] != neighbour
                     # Check whether the loop has closed
                     if neighbour == tail
-                        # println("Setting left_parents[", tail, "] = ", node)
                         left_parents[tail] = node
                         # Return true if have found a cycle
                         return true
@@ -186,29 +181,6 @@ function find_cycle(tail, head, left, right, left_parents, right_parents, node_q
     return false
 end
 
-# Disregard this function
-# Given left_parents, right_parents and a node i on the cycle returns a list of the
-# nodes making up the cycle.
-function get_cycle(i, left_parents, right_parents, max_iter = 10000)
-    cycle = Vector{UInt32}()
-    push!(cycle, i)
-    curr = left_parents[i]
-    # start iteration on right side
-    side = 1
-    while !(curr == i && side == 0) && max_iter > 0
-        max_iter -= 1
-        push!(cycle, curr)
-        # println((curr, side, i, side))
-        if side == 0
-            curr = left_parents[curr]
-        else
-            curr = right_parents[curr]
-        end
-
-        side = 1 - side
-    end
-    return cycle
-end
 
 # Calculates the minimum value of P[i,j] along the reverse edges of the detected cycle.
 function calculate_improvement(i, j, left_parents, right_parents, P, res, max_iter = 10000)
@@ -218,12 +190,9 @@ function calculate_improvement(i, j, left_parents, right_parents, P, res, max_it
     imp_i = curr_i
     imp_j = curr_j
     curr_i = right_parents[curr_j]
-    # println((curr_i, curr_j))
 
     while curr_i != i && max_iter > 0
         max_iter -= 1
-        # println((curr_i, curr_j))
-        # println("Checking edge ", (curr_i, curr_j, P[curr_i, curr_j]))
         curr_j = left_parents[curr_i]
         if P[curr_i, curr_j] < imp
             imp_i = curr_i
@@ -237,12 +206,11 @@ function calculate_improvement(i, j, left_parents, right_parents, P, res, max_it
     return imp
 end
 
-
-
+# Given starting arc (i,j) improvement imp, and leaving edge (imp_i, imp_j), updates
+# the tree structure in that it: 1) updates P using 'imp' 2) removes leaving edge.
 function update_tree(i, j, imp, imp_i, imp_j, P, left, right, left_parents, right_parents, max_iter = 10000)
     curr_i = i
     curr_j = j
-    # println((curr_i, curr_j))
     P[curr_i, curr_j] += imp
     curr_j = left_parents[curr_i]
     P[curr_i, curr_j] -= imp
@@ -258,12 +226,14 @@ function update_tree(i, j, imp, imp_i, imp_j, P, left, right, left_parents, righ
     delete!(right[imp_j], imp_i)
 end
 
-
+# Calculates inner product between two commensurate matrices.
 function objective(P, C)
     return vec(P)'*vec(C)
 end
 
-# runs <= 100 iterations of network simplex on measures a,b, with cost matrix C
+
+# Returns optimal coupling between measures a,b wrt cost matrix C using Network Simplex.
+# Block search is used with block size = α ⋅ m ⋅ n .
 function my_emd(a, b, C; α = .05, max_iter = 10000)
     n = length(a)
     m = length(b)
@@ -281,14 +251,17 @@ function my_emd(a, b, C; α = .05, max_iter = 10000)
         right[j] = BitSet()
     end
 
-    # Initializing the coupling matrix.
+    # Allocating memory for coupling matrix
     P = zeros(n,m)
 
     # Initializing potentials
     u = zeros(n)
     v = zeros(m)
 
+    # Initializing coupling matrix
     NorthWest!(left, right, P, a, b)
+
+    # Allocating memory for cycle search
     left_parents = Vector{UInt32}(undef, n)
     right_parents = Vector{UInt32}(undef, m)
     enter_i = -1
@@ -296,24 +269,26 @@ function my_emd(a, b, C; α = .05, max_iter = 10000)
     node_queue = Vector{UInt32}(undef, n+m)
     side_queue = Vector{Bool}(undef, n+m)
 
+    # Variables for block search
     blk_size = max(10, floor(α*m*n))
     blk_start = 1
 
+    # Initializing variable to be used in update_potential()
     unchanged = Vector{Bool}(undef, n)
     fill!(unchanged, true)
+
+    # Cache memory
     res = [0,0,0]
     res_float = [.0, .0]
+
+    # Keep track of edges that have already been tried and don't improve objective
     bad_i = Vector{UInt32}(undef, 20)
     bad_j = Vector{UInt32}(undef, 20)
     bad_index = 0
 
     while true && max_iter > 0
         max_iter -= 1
-        # println(objective(P, C))
-        updatePotential!(u, v, C, left, right, unchanged)
-        #
-        # Testing wheter updatePotential works
-        # println("Potential update error = ", sum(abs2, (P .!= 0).*(C - (u.+v'))))
+        update_potential!(u, v, C, left, right, unchanged)
 
         prev_i = enter_i
         prev_j = enter_j
@@ -325,25 +300,17 @@ function my_emd(a, b, C; α = .05, max_iter = 10000)
         blk_start = (blk_start - 1)%(m*n) + 1
 
         if done > 0
-            # println("Exited with done > 0")
             return P
         end
-
-        # println(objective(P, C), (enter_i, enter_j))
 
         # Add entering edge to tree (creating a unique cycle)
         push!(left[enter_i], enter_j)
         push!(right[enter_j], enter_i)
         edges = 0
-        # for i in 1:n
-        #     edges += length(left[i])
-        # end
-        # println("Currently ", edges, "/", m + n, " edges present")
 
         right_parents[enter_j] = enter_i
         is_cycle = find_cycle(enter_i, enter_j, left, right, left_parents, right_parents, node_queue, side_queue)
 
-        # cycle = get_cycle(enter_i, left_parents, right_parents)
         if is_cycle
             bad_index = 0
             imp = calculate_improvement(enter_i, enter_j, left_parents, right_parents, P, res)
@@ -362,10 +329,11 @@ function my_emd(a, b, C; α = .05, max_iter = 10000)
             end
         end
     end
-    println("ran out of iterations")
+    println("Reached iteration limit")
     return P
 end
 
+# Creates random cost-matrix inplace, using i.i.d. d-dimensional Gaussian vectors
 function random_cost(n,m,C;d=3)
     x_points = randn(n,d)
     y_points = randn(m, d)
@@ -376,6 +344,7 @@ function random_cost(n,m,C;d=3)
     end
 end
 
+# Tests my_emd() over a range of synthetic inputs
 function test()
     # Size of support of two measures. n corrseponds to rows and m to columns.
     ns = [3,5,10,15,29,47, 99]
@@ -408,7 +377,12 @@ function test()
     return true
 end
 
-# Performance analysis
+# Test does not always pass. When it doesn't it is due to: gets stuck in local optima because
+# non-strongly feasible entering variables are used (see Bertsekas: Network Optimization (1997) page 183)
+# and algorithm reaches iteration limit
+test()
+
+###################### PERFORMANCE ANALYSIS
 const to = TimerOutput()
 
 grid = Int.(round.(exp.(log(4):0.2:log(750))))
@@ -439,6 +413,7 @@ for j in 1:length(grid)
 end
 
 
+#################### BLOCK SEARCH BLOCK SIZE EXPERIMENT
 αs = round.(exp.(log(0.01):.1:0), digits = 3)
 n = 30
 n_average = 600
@@ -463,11 +438,11 @@ for i in 1:length(αs)
 end
 
 
+
+########################### PLOTTING
 plot(grid, log.(times), xaxis = "n = m", yaxis = "log(nanoseconds)", title = "Running times", label = ["Ours" "C++"])
 savefig("times.png")
 plot(grid, log.(mem_usage./10^6), xaxis = "n = m", yaxis = "log(Gigabytes)", title = "Memory usage", label = ["Ours" "C++"])
 savefig("memory.png")
 plot(αs, log.(α_times), title="Block size", xaxis="α", yaxis="log(nanosecons)", label=nothing)
 savefig("α.png")
-
-3+3
